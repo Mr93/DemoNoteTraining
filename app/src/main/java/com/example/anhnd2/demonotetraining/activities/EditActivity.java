a2c88cc09e0e4c9f19dd8b601b08a5ca502dd278
ANHND2\anhnd2 <anhdn@rikkeisoft.com>
2017-07-10 18:19:33 +0700

    create edit activity, mvp edit

--- app/src/main/java/com/example/anhnd2/demonotetraining/activities/EditActivity.java
+++ app/src/main/java/com/example/anhnd2/demonotetraining/activities/EditActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.anhnd2.demonotetraining.R;
 
@@ -18,5 +20,26 @@ public class EditActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_edit, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+			case R.id.menu_capture:
+				break;
+			case R.id.menu_chose_color:
+				break;
+			case R.id.menu_save:
+				break;
+			default:
+				break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
---
+++ app/src/main/java/com/example/anhnd2/demonotetraining/interfaces/MvpEdit.java
@@ -0,0 +1,25 @@
+package com.example.anhnd2.demonotetraining.interfaces;
+
+/**
+ * Created by anhnd2 on 7/10/2017.
+ */
+
+public interface MvpEdit {
+
+	interface ProvidedPresenter{
+
+	}
+
+	interface ProvidedModel{
+
+	}
+
+	interface RequiredPresenter{
+
+	}
+
+	interface RequiredView{
+
+	}
+
+}
